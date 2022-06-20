71cde4bdda59e0f0c94019401149f955d3ae0bd1
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-06-20 18:46:18 -0300

    Atividade 02

---
+++ 02-if_else/script.js
@@ -0,0 +1,24 @@
+const idade = 18;
+if (idade >= 18) {
+  console.log("18 years is " + true);
+}
+
+const mes = "Janeiro";
+if (mes === "Janeiro" || mes === "Dezembro") {
+  console.log("Parabens");
+} else {
+  console.log("Parabens");
+}
+
+const nome = "Lucas";
+if (nome[0] === "R") {
+  console.log("Seu nome começa com R");
+} else {
+  console.log("Seu nome não começa com R");
+}
+
+if (nome.length >= 6) {
+  console.log("Seu nome é maior que 6");
+} else {
+  console.log("Seu nome é menor que 6");
+}
