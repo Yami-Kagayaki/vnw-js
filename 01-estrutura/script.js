b7f49f0cd8e6f1d6ac95c11f85085e9812f651a8
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-06-20 18:35:34 -0300

    Atividade 01

---
+++ 01-estrutura/script.js
@@ -0,0 +1,8 @@
+const name = "Luska";
+const num = 1;
+const date = 2022 - 1;
+const quartaFeira = true;
+
+console.log(date + num);
+console.log("Estou mexendo no console.log");
+console.log(quartaFeira);
