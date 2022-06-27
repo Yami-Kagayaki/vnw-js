874408c47fa4c020643452c2db6f70004aa2dc48
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-06-27 18:35:09 -0300

    atividade 05

---
+++ 05-estrutura_de_dados_complexos/script.js
@@ -0,0 +1,20 @@
+const array = ["a", 1, "b", 2, "c", 3];
+
+console.log(array);
+
+array.unshift("OPA");
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.push("P");
+console.log(array);
+
+array.shift();
+console.log(array);
+
+const numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+console.log(numbers);
+numbers.sort((a, b) => a - b);
+console.log(numbers);
